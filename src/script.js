f184309556ff9f485ce66c6a98a44fdb90a3c8aa
zlargon <zlargon1988@gmail.com>
2019-06-01 16:03:12 -0400

    load the data and create the options

--- src/script.js
+++ src/script.js
@@ -21,3 +21,67 @@ video.addEventListener('timeupdate', () => {
     btn.className = 'play';
   }
 });
+
+const createDecisionBox = ({
+    decisionTime = 10, // default 10
+    onMakeDecision = console.log,
+    options = [
+      { next: '001', value: 'option 1' },
+      { next: '002', value: 'option 2' },
+      { next: '003', value: 'option 3' },
+      { next: '004', value: 'option 4' }
+    ]
+  }) => {
+
+  // choiceBar
+  const choiceBar = document.getElementById('choice-bar');
+
+  // <div class="option">
+  //   <div>option 1</div>
+  //   <div class="underline effect"></div>
+  // </div>
+  for (const opt of options) {
+    const option = document.createElement('div');
+    option.className = 'option';
+    option.innerHTML = `<div class="underline effect"></div>`
+
+    const value = document.createElement('div');
+    value.innerText = opt.value;
+    value.onclick = () => {
+      console.log(opt);
+    }
+
+    option.prepend(value);
+    choiceBar.appendChild(option);
+  }
+
+  // show and hide
+  let isShow = false;
+  const show = () => {
+    isShow = true;
+    choiceBar.style.transform = 'translateY(0)';
+  }
+  const hide = () => {
+    isShow = false;
+    choiceBar.style.transform = 'translateY(100%)';
+  }
+
+  return {
+    show,
+    hide,
+    toggle: () => {
+      if (isShow) {
+        hide();
+      } else {
+        show();
+      }
+    }
+  }
+}
+
+// test
+const testButton = document.getElementById('test');
+const decisionBox = createDecisionBox({});
+testButton.onclick = () => {
+  decisionBox.toggle();
+}
