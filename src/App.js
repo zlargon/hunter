7e0b43c06dba743462c21afe6c17acbab4a638a6
zlargon <zlargon1988@gmail.com>
2019-06-05 19:30:27 -0400

    add video with video handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import movie from './video.mp4';
+import './App.css';
 
 const App = () => {
+
+  const videoHandler = ({ target: vid }) => {
+    console.dir(vid);
+    console.log('currentTime:', vid.currentTime);
+    console.log('duration:', vid.duration);
+
+    if (vid.currentTime >= 10) {
+      vid.currentTime = 0;
+    }
+  }
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <div className="app-video-section">
+        <video style={{ width: '100%' }}
+          src={movie}
+          onTimeUpdate={videoHandler}
+          controls>
+        </video>
+      </div>
     </div>
   );
 }
