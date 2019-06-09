8402257b2a648796b4d70f7b0700214d2f6ef884
zlargon <zlargon1988@gmail.com>
2019-06-09 16:33:21 -0400

    update README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Hunter
+# The Hunter
+
+An Interactive Film
+
+# Story Flow
+
+https://www.lucidchart.com/invitations/accept/44e6f3f8-864e-4875-902b-7a9a0b526f52
+
+```js
+"002": {                                  // clip's unique name
+  "source": "video.mp4",                  // video source
+  "description": "",                      // description of the clip
+  "start_time": "00:01:18.171",           // the start time of the clip
+  "end_time": "00:01:20.991",             // the end time of the clip
+  "question": null,
+  "question": "Let Sam ...",              // decision question
+  "options": [                            // the choice for making decision
+    { "next": "004", "value": "Speak" },  // next clip's name and the decision's answer
+    { "next": "003", "value": "Stay Quite" }
+  ]
+}
+```
--- src/storyflow.js
+++ src/storyflow.js
@@ -4,13 +4,13 @@ const StoryFlow = {
     "description": ""
   },
 
-  // 0 ~ 20
+  // 0 ~ 30
   "001": {
     "start_time": 0,
-    "end_time": 20,
+    "end_time": 30,
     "options": [
-      { "next": "001", "value": "goto 0" },
       { "next": "002", "value": "goto 30" },
+      { "next": "001", "value": "goto 0" },
     ]
   },
 
@@ -19,24 +19,27 @@ const StoryFlow = {
     "start_time": 30,
     "end_time": 60,
     "options": [
-      { "next": "001", "value": "goto 0" },
       { "next": "003", "value": "goto 60" },
+      { "next": "002", "value": "goto 30" },
+      { "next": "001", "value": "goto 0" },
     ]
   },
 
-  // 60 ~ 80
+  // 60 ~ 90
   "003": {
     "start_time": 60,
-    "end_time": 80,
+    "end_time": 90,
     "options": [
+      { "next": "004", "value": "goto 90" },
+      { "next": "003", "value": "goto 60" },
+      { "next": "002", "value": "goto 30" },
       { "next": "001", "value": "goto 0" },
-      { "next": "004", "value": "goto 100" },
     ]
   },
 
-  // 100 ~
+  // 90 ~
   "004": {
-    "start_time": 100,
+    "start_time": 90,
     "end_time": null
   }
 };
