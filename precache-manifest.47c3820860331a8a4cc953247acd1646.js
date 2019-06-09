0dad474a1684a1f278ccbd08a0273386ab2afb4d
zlargon <zlargon1988@gmail.com>
2019-06-09 17:23:07 -0400

    Updates

---
+++ precache-manifest.47c3820860331a8a4cc953247acd1646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8233f96b2449cac64f300e7ffbf03a44",
+    "url": "/hunter/index.html"
+  },
+  {
+    "revision": "df82f13f3857c2a9fe81",
+    "url": "/hunter/static/css/main.56da57ca.chunk.css"
+  },
+  {
+    "revision": "6bea2a1750d5e71fa6f7",
+    "url": "/hunter/static/js/2.91120321.chunk.js"
+  },
+  {
+    "revision": "df82f13f3857c2a9fe81",
+    "url": "/hunter/static/js/main.c250f334.chunk.js"
+  },
+  {
+    "revision": "8530f35293ded0a6b4ca",
+    "url": "/hunter/static/js/runtime~main.250b9548.js"
+  }
+]);
