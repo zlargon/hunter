7cb8b099ebfe9992ec2a77ad27c5603fe2b9c0c7
zlargon <zlargon1988@gmail.com>
2019-06-09 17:21:59 -0400

    edit story flow to show decision box every 30 sec

--- src/storyflow.js
+++ src/storyflow.js
@@ -9,8 +9,9 @@ const StoryFlow = {
     "start_time": 0,
     "end_time": 30,
     "options": [
-      { "next": "002", "value": "goto 30" },
-      { "next": "001", "value": "goto 0" },
+      { "next": "003", "value": "01:00" },
+      { "next": "002", "value": "00:30" },
+      { "next": "001", "value": "00:00" },
     ]
   },
 
@@ -19,9 +20,9 @@ const StoryFlow = {
     "start_time": 30,
     "end_time": 60,
     "options": [
-      { "next": "003", "value": "goto 60" },
-      { "next": "002", "value": "goto 30" },
-      { "next": "001", "value": "goto 0" },
+      { "next": "004", "value": "01:30" },
+      { "next": "003", "value": "01:00" },
+      { "next": "002", "value": "00:30" },
     ]
   },
 
@@ -30,18 +31,71 @@ const StoryFlow = {
     "start_time": 60,
     "end_time": 90,
     "options": [
-      { "next": "004", "value": "goto 90" },
-      { "next": "003", "value": "goto 60" },
-      { "next": "002", "value": "goto 30" },
-      { "next": "001", "value": "goto 0" },
+      { "next": "005", "value": "02:00" },
+      { "next": "004", "value": "01:30" },
+      { "next": "003", "value": "01:00" },
     ]
   },
 
-  // 90 ~
+  // 90 ~ 120
   "004": {
     "start_time": 90,
+    "end_time": 120,
+    "options": [
+      { "next": "006", "value": "02:30" },
+      { "next": "005", "value": "02:00" },
+      { "next": "004", "value": "01:30" },
+    ]
+  },
+
+  // 120 ~ 150
+  "005": {
+    "start_time": 120,
+    "end_time": 150,
+    "options": [
+      { "next": "007", "value": "03:00" },
+      { "next": "006", "value": "02:30" },
+      { "next": "005", "value": "02:00" },
+    ]
+  },
+
+  // 150 ~ 180
+  "006": {
+    "start_time": 150,
+    "end_time": 180,
+    "options": [
+      { "next": "008", "value": "03:30" },
+      { "next": "007", "value": "03:00" },
+      { "next": "006", "value": "02:30" },
+    ]
+  },
+
+  // 180 ~ 210
+  "007": {
+    "start_time": 180,
+    "end_time": 210,
+    "options": [
+      { "next": "009", "value": "04:00" },
+      { "next": "008", "value": "03:30" },
+      { "next": "007", "value": "03:00" },
+    ]
+  },
+
+  // 210 ~ 240
+  "008": {
+    "start_time": 210,
+    "end_time": 240,
+    "options": [
+      { "next": "009", "value": "04:00" },
+      { "next": "008", "value": "03:30" },
+    ]
+  },
+
+  // 240
+  "009": {
+    "start_time": 240,
     "end_time": null
-  }
+  },
 };
 
 export default StoryFlow;
